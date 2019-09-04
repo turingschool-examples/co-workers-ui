b816d42fdc6c0dbcdda2ee10b2ed0d42a5636ef7
Travis Rollins <troll59910@aol.com>
2019-09-04 17:04:39 -0600

    Remove extra try catches in apiCalls file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,15 +1,11 @@
 export const getCoWorkers = async () => {
   const url = 'http://localhost:3001/api/v1/coworkers'
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('There was an error getting your co-workers.');
-    }
-    const coWorkers= await response.json();
-    return coWorkers 
-  } catch(error) {
-    throw new Error(error.message);
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('There was an error getting your co-workers.');
   }
+  const coWorkers = await response.json();
+  return coWorkers
 }
 
 export const createCoWorker = async newCoWorker => {
@@ -21,32 +17,24 @@ export const createCoWorker = async newCoWorker => {
     },
     body: JSON.stringify({ ...newCoWorker })
   };
-  try {
-    const response  = await fetch(url, options);
-    if (!response.ok) {
-      throw new Error('Sorry.  Unable to create your new co-worker.');
-    }
-    const newCoWorkerId = await response.json();
-    return newCoWorkerId;
-  } catch(error) {
-    throw new Error(error.message);
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error('Sorry.  Unable to create your new co-worker.');
   }
+  const newCoWorkerId = await response.json();
+  return newCoWorkerId;
 }
 
 export const deleteCoWorker = async id => {
-  const url  = `http://localhost:3001/api/v1/coworkers/${id}`;
+  const url = `http://localhost:3001/api/v1/coworkers/${id}`;
   const options = {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json'
     }
   }
-  try {
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      throw new Error('There was a problem removing this co-worker.')
-    }
-  } catch(error) {
-    throw new Error(error.message)
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error('There was a problem removing this co-worker.')
   }
 }
